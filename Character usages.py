84fe5f87254de34bf28669fb8a3f51d4840a3cac
Megan Leet <megan.leet@iinet.net.au>
2023-11-28 10:09:24 +1100

    mwhahahahaha

--- Character usages.py
+++ Character usages.py
@@ -9,7 +9,7 @@ from datetime import date
 from pathlib import Path
 from typing import cast
 
-from ausmash import Character, Match, combine_echo_fighters
+from ausmash import BracketStyle, Character, Match, Region, combine_echo_fighters
 from matplotlib import pyplot
 from pyzstd import ZstdFile
 from tqdm import tqdm
@@ -21,7 +21,7 @@ logger = logging.getLogger(__file__)
 ssbu_chars = {combine_echo_fighters(char) for char in Character.characters_in_game('SSBU')}
 
 
-def character_usages(matches: Sequence[Match]):
+def character_usages(matches: Sequence[Match]) -> tuple[float, list[TieredItem[Character]]]:
 	usages: defaultdict[Character, int] = defaultdict(int)
 	matches_with_character_data = 0
 	for match in tqdm(matches):
@@ -34,40 +34,85 @@ def character_usages(matches: Sequence[Match]):
 		for char in chars_used:
 			usages[char] += 1
 
-	logger.info(
-		'Ratio of matches with character data: %g %g',
-		matches_with_character_data / len(matches),
-		len(matches) / matches_with_character_data,
-	)
+	if matches[0].game.short_name == 'SSBU':
+		for char in ssbu_chars:
+			usages[char]
 
-	for char in ssbu_chars:
-		usages.__getitem__(char)
-
-	return (TieredItem(char, usage / len(matches)) for char, usage in usages.items())
+	return matches_with_character_data / len(matches), [
+		TieredItem(char, usage / len(matches)) for char, usage in usages.items()
+	]
 
 
 def months_between(d1: date, d2: date) -> int:
 	return ((d1.year - d2.year) * 12) + (d1.month - d2.month)
 
 
-def _region(match: Match) -> str:
+def _region_name(match: Match) -> str:
 	return match.tournament.region.short_name
+
+
+def _region(match: Match) -> Region:
+	return match.tournament.region
 
 
 colourmap = pyplot.get_cmap('Spectral')
 
 
-def output_tier_list(matches: Sequence[Match], image_path: Path):
-	usages = character_usages(matches)
+def output_tier_list(
+	region: Region | None,
+	matches: Sequence[Match],
+	image_path: Path,
+	*additional_title_lines: str,
+):
+	ratio, usages = character_usages(matches)
+	match_dates = [m.date for m in matches]
+	earliest, latest = min(match_dates), max(match_dates)
+	title_lines = [
+		region.name if region else 'Australia + New Zealand',
+	]
+	if additional_title_lines:
+		title_lines += additional_title_lines
+	title_lines += [
+		f'From {earliest} to {latest}',
+		f'{ratio:%} of {len(matches)} matches with character data',
+	]
 	tier_list = CharacterTierList(
 		usages,
+		title='\n'.join(title_lines),
 		score_formatter='%',
 		scale_factor=3,
 	)
-	if tier_list.num_tiers == 3:
+	if tier_list.num_tiers == 2:
+		tier_list.tier_names = dict(enumerate(('Used', 'Less used')))
+	elif tier_list.num_tiers == 3:
 		tier_list.tier_names = dict(enumerate('SAB'))
-	tier_list.to_image(colourmap, show_scores=True).save(image_path)
+	tier_list.to_image(
+		colourmap, show_scores=True, title_background=region.colour_string if region else None
+	).save(image_path)
 	image_path.with_suffix('.txt').write_text(tier_list.to_text(show_scores=True), encoding='utf8')
+
+
+def _is_in_grands(match: Match) -> bool:
+	if match.event not in match.tournament.events:
+		logger.error('what the hellll %s %s %s', match, match.event, match.tournament)
+		return False
+	return (
+		match.round_name in {'GF', 'GF2'}
+		and not match.event.is_redemption_bracket
+		and match.tournament.final_phase_for_event(match.event) == match.event
+	)
+
+
+def _is_in_wr1_or_2(match: Match) -> bool:
+	# TODO: What I really want is "is in starting round" but I'd have to look at start.gg to see who starts in WR1 and who starts in WR2 (or calculate that from seeding which needs looking at start.gg anyway)
+	if match.event not in match.tournament.events:
+		logger.error('what the hellll %s %s %s', match, match.event, match.tournament)
+		return False
+	return (
+		(match.round_name in {'W1', 'W2'} or match.event.bracket_style == BracketStyle.RoundRobin)
+		and not match.event.is_redemption_bracket
+		and match.tournament.start_phase_for_event(match.event) == match.event
+	)
 
 
 def main() -> None:
@@ -76,29 +121,85 @@ def main() -> None:
 		matches = cast(Sequence[Match], pickle.load(z))
 
 	ult_matches = sorted(
-		(match for match in matches if match.game.short_name == 'SSBU'), key=_region
+		(match for match in matches if match.game.short_name == 'SSBU'), key=_region_name
 	)
-	output_tier_list(ult_matches, base_dir / 'Characters by usage.png')
+	output_tier_list(None, ult_matches, base_dir / 'Characters by usage.png')
 	output_tier_list(
+		None,
+		[m for m in ult_matches if _is_in_wr1_or_2(m)],
+		base_dir / 'Characters by usage in WR1 or WR2.png',
+		'Winners round 1 or 2',
+	)
+	output_tier_list(
+		None,
+		[m for m in ult_matches if _is_in_grands(m)],
+		base_dir / 'Characters by usage in grands.png',
+		'Grand finals',
+	)
+	output_tier_list(
+		None,
 		[match for match in matches if match.game.short_name == 'SSBM'],
 		base_dir / 'SSBM characters by usage.png',
 	)
+	recent_matches = [m for m in ult_matches if months_between(date.today(), m.date) <= 3]
 	output_tier_list(
-		[m for m in ult_matches if months_between(date.today(), m.date) <= 3],
+		None,
+		recent_matches,
 		base_dir / 'Characters by usage in last 3 months.png',
+	)
+	output_tier_list(
+		None,
+		[m for m in recent_matches if _is_in_wr1_or_2(m)],
+		base_dir / 'Characters by usage in WR1 or WR2 in last 3 months.png',
+		'Winners round 1 or 2',
+	)
+	output_tier_list(
+		None,
+		[m for m in recent_matches if _is_in_grands(m)],
+		base_dir / 'Characters by usage in grands in last 3 months.png',
+		'Grand finals',
 	)
 
 	for region, group in itertools.groupby(ult_matches, _region):
-		logger.info(region)
+		logger.info(region.short_name)
 		matches = list(group)
-		output_tier_list(matches, base_dir / f'Characters by usage in {region}.png')
+		output_tier_list(
+			region, matches, base_dir / f'Characters by usage in {region.short_name}.png'
+		)
+		output_tier_list(
+			region,
+			[m for m in matches if _is_in_wr1_or_2(m)],
+			base_dir / f'Characters by usage in WR1 or WR2 in {region.short_name}.png',
+			'Winners round 1 or 2',
+		)
+		output_tier_list(
+			region,
+			[m for m in matches if _is_in_grands(m)],
+			base_dir / f'Characters by usage in grands in {region.short_name}.png',
+			'Grand finals',
+		)
 
 		recent_matches = [m for m in matches if months_between(date.today(), m.date) <= 3]
 		if not recent_matches:
-			logger.info('wank dicks balls piss')
+			logger.info('wank dicks balls piss %s has no active matches', region.name)
 			continue
 		output_tier_list(
-			recent_matches, base_dir / f'Characters by usage in {region} in last 3 months.png'
+			region,
+			recent_matches,
+			base_dir / f'Characters by usage in {region.short_name} in last 3 months.png',
+		)
+		output_tier_list(
+			region,
+			[m for m in recent_matches if _is_in_wr1_or_2(m)],
+			base_dir
+			/ f'Characters by usage in WR1 or WR2 in {region.short_name} in last 3 months.png',
+			'Winners round 1 or 2',
+		)
+		output_tier_list(
+			region,
+			[m for m in recent_matches if _is_in_grands(m)],
+			base_dir / f'Characters by usage in grands in {region.short_name} in last 3 months.png',
+			'Grand finals',
 		)
 
 
